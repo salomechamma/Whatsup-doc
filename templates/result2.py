60c3f7d83ac7264a2104f57c948e35ddbcdc1a95
Salome Chamma <salome.chamma@gmail.com>
2016-11-12 01:27:41 +0000

    Added user pages (login, sign in etc..)

---
+++ templates/result2.py
@@ -0,0 +1,11 @@
+    
+<!-- {% if search_results %} -->
+        <!-- (% for doc in search_results %) -->
+           <!-- <div> {{ doc['physician_first_name'] }} </div>  -->
+           <!-- <div> {{ doc['physician_profile_id'] }} </div>  -->
+        <!-- (% endfor %) -->
+
+
+    <!-- {% else %} -->
+    <!-- <div> No Doctor match, please try again. </div> -->
+   <!--  {% endif %} -->
---
+++ tests.py
@@ -0,0 +1,123 @@
+import json
+from unittest import TestCase
+from model import Doctor, User, Like, connect_to_db, db
+from server import app
+import server
+
+
+class FlaskTestsBasic(TestCase):
+    """Flask tests."""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        # Get the Flask test client
+        self.client = app.test_client()
+
+        # Show Flask errors that happen during tests
+        app.config['TESTING'] = True
+
+    def test_index(self):
+        """Test homepage page."""
+
+        result = self.client.get("/")
+        self.assertEqual(result.status_code,200)
+        self.assertIn("Search", result.data)
+
+
+    def test_result_page(self):
+        """Test results page."""
+
+        result = self.client.get("/results_list", data={'firstname':'charles', 'lastname':'saha'})
+        self.assertIn("Search", result.data)
+
+    
+
+# class FlaskTestsDatabase(TestCase):
+#     """Flask tests that use the database."""
+
+#     def setUp(self):
+#         """Stuff to do before every test."""
+
+#         # Get the Flask test client
+#         self.client = app.test_client()
+#         app.config['TESTING'] = True
+
+#         # Connect to test database
+#         connect_to_db(app, "postgresql:///testdb")
+
+#         # Create tables and add sample data
+#         db.create_all()
+#         example_data()
+
+#     def tearDown(self):
+#         """Do at end of every test."""
+
+#         db.session.close()
+#         db.drop_all()
+
+#     def test_departments_list(self):
+#         """Test departments page."""
+
+#         result = self.client.get("/departments")
+#         self.assertIn("Legal", result.data)
+
+
+#     def test_departments_details(self):
+#         """Test departments page."""
+
+#         result = self.client.get("/department/fin")
+#         self.assertIn("Phone: 555-1000", result.data)
+
+
+#     def test_login(self):
+#         """Test login page."""
+
+#         result = self.client.post("/login", 
+#                                   data={"user_id": "rachel", "password": "123"},
+#                                   follow_redirects=True)
+#         self.assertIn("You are a valued user", result.data)
+
+
+# class FlaskTestsLoggedIn(TestCase):
+#     """Flask tests with user logged in to session."""
+
+#     def setUp(self):
+#         """Stuff to do before every test."""
+
+#         app.config['TESTING'] = True
+#         app.config['SECRET_KEY'] = 'key'
+#         self.client = app.test_client()
+
+#         with self.client as c:
+#             with c.session_transaction() as sess:
+#                 sess['user_id'] = 1
+
+#     def test_important_page(self):
+#         """Test important page."""
+
+#         result = self.client.get("/important")
+#         self.assertIn("You are a valued user", result.data)
+
+
+# class FlaskTestsLoggedOut(TestCase):
+#     """Flask tests with user logged in to session."""
+
+#     def setUp(self):
+#         """Stuff to do before every test."""
+
+#         app.config['TESTING'] = True
+#         self.client = app.test_client()
+
+#     def test_important_page(self):
+#         """Test that user can't see important page when logged out."""
+
+#         result = self.client.get("/important", follow_redirects=True)
+#         self.assertNotIn("You are a valued user", result.data)
+#         self.assertIn("You must be logged in", result.data)
+
+
+if __name__ == "__main__":
+    import unittest
+
+    unittest.main()
